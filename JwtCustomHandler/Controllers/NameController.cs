a9a59b90168f85c41184e044c374b749cd2eeb4b
Abid Hassan <abidhassandiba@gmail.com>
2021-09-18 19:41:59 +0600

    Add project files.

---
+++ JwtCustomHandler/Controllers/NameController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JwtCustomHandler.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        private readonly ICustomAuthenticationManager customAuthenticationManager;
+
+        public NameController(ICustomAuthenticationManager customAuthenticationManager)
+        {
+            this.customAuthenticationManager = customAuthenticationManager;
+        }
+
+        // GET: api/Name
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Khulna", "Jessore" };
+        }
+
+        // GET: api/Name/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "New Jersey";
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] UserCred userCred)
+        {
+            var token = customAuthenticationManager.Authenticate(userCred.Username, userCred.Password);
+
+            if (token == null)
+                return Unauthorized();
+
+            return Ok(token);
+        }
+    }
+}
---
+++ JwtCustomHandler/ICustomAuthenticationManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtCustomHandler
+{
+    public interface ICustomAuthenticationManager
+    {
+        string Authenticate(string username, string password);
+
+        IDictionary<string, string> Tokens { get; }
+    }
+}
